3a6e5c430cf3cf30a086270cca5b3c10d96cc66e
andika rifki <andikarifki03@gmail.com>
2025-10-10 11:57:45 +0700

    update login and header

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,19 @@
     <div class="max-w-6xl mx-auto bg-white p-6 sm:p-8 rounded-xl shadow-2xl">
 
       <header class="flex justify-between items-center mb-6 border-b-4 border-blue-200 pb-3">
-        <h1 class="text-4xl font-extrabold text-blue-700 text-center flex-grow">
-          Klien API Pasien & Riwayat Medis
-        </h1>
+        <div class="flex items-center">
+          <img
+            src="https://img.okadoc.com/plain/200x200/photos/practitioner/photo/73503/okadoc-doctor-psychiatrist-johan-kurniawan-20200420165834.jpg"
+            alt="Foto Dr. Johan" class="w-16 h-16 rounded-full object-cover shadow-xl border-2 border-blue-200 mr-4" />
+          <div>
+            <h1 class="text-2xl font-extrabold text-blue-800">
+              Manajemen Praktik dr. Johan
+            </h1>
+            <p class="text-gray-600 text-sm">
+              Spesialis Kedokteran Jiwa atau Psikiatri
+            </p>
+          </div>
+        </div>
       </header>
 
       <router-view></router-view>
